b784bd0220a532a24923fdfc5c2efe7684b0cd48
Magnific86 <klevtsov10012002@gmail.com>
2022-12-01 01:46:51 +0500

    upd

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3aaec23a7240af98"; }
+/******/ 		__webpack_require__.h = function() { return "ddcb19e14f49bca3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
